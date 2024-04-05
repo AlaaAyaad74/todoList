3539db20b6f7011d947f87af9ea0e6fa7af97158
Alaa Ayaad <alaaayad1@outlook.com>
2024-04-05 18:49:28 +0200

    first commit

--- src/components/column/ColumnStyle.jsx
+++ src/components/column/ColumnStyle.jsx
@@ -6,7 +6,7 @@ export default styled.div`
     font-size: 1.1rem;
     color: var(--text-color);
 
-    justify-content: flex-start;
+    justify-content: center;
     width: 100%;
   }
   .Title_column div {
@@ -15,5 +15,10 @@ export default styled.div`
     border-radius: 50%;
     background-color: green;
     margin-inline-end: 10px;
+  }
+  @media (max-width: 810px) {
+    .Title_column {
+      justify-content: flex-start;
+    }
   }
 `;
